987b154a9219cc8e3be9eaf004d2fe211f45d540
Arodriguez1030 <arodriguez1030@gmail.com>
2021-04-21 11:46:08 -0400

    updated category route and controller to include create method

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -3,4 +3,13 @@ class Api::V1::CategoriesController < ApplicationController
         categories = Category.all
         render json: CategorySerializer.new(categories)
     end
+
+    def create
+       category Category.new(video_params)
+        if category.save
+            render json: CategorySerializer.new(video), status: :accepted
+        else
+            render json: {errors: category.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
 end
