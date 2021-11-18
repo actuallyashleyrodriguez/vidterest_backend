36e6288519f4b5a5fa30697f33a774ac02a59626
Arodriguez1030 <arodriguez1030@gmail.com>
2021-11-18 12:43:26 -0500

    datadog changes

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -1,2 +1,12 @@
 Datadog.configure do |c|
+    c.use :rails
+
+    c.tracer sampler: Datadog::PrioritySampler.new(
+post_sampler: Datadog::Sampling::RuleSampler.new(
+    [
+        # Sample all 'vidterest_backend' traces at 50.00%:
+        Datadog::Sampling::SimpleRule.new(service: 'vidterest_backend', sample_rate: 0.5000)
+    ]
+)
+)
 end
